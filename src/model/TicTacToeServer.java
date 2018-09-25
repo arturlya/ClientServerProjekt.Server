406b23eb0e9c593f46a21d447f8324665e64ee38
Artur Lyadskyy <lyadskyyartur@gmail.com>
2018-09-25 18:52:46 +0200

    Update

--- src/model/TicTacToeServer.java
+++ src/model/TicTacToeServer.java
@@ -3,28 +3,42 @@ package model;
 import model.abitur.netz.Client;
 import model.abitur.netz.Server;
 
+/**
+ * Klasse des Servers zum TicTacToe spielen.
+ */
 public class TicTacToeServer extends Server {
 
+    /** Anzahl der Clients auf dem Server*/
     int numberOfClients;
 
-    boolean player1turn;
+    /** Boolische Werte, die angeben, ob ein Spieler gewonnen hat oder Spieler 1 am Zug ist.*/
+    boolean player1turn,playerWon;
 
+    /** Das Spielfeld, das in einem 2-Dimensionalen Array gespeichert wird*/
     Field[][] map;
 
-
-
+    /**
+     * Konstruktor der Klasse TicTacToeServer.
+     * Erstellt einen Server auf dem Clients TicTacToe spielen können.
+     *
+     * @param port Port, den der Server belegt.
+     */
     public TicTacToeServer(int port){
         super(port);
         System.out.println("running Server");
         numberOfClients =0;
+        playerWon = false;
         map = new Field[3][3];
         createMap();
-
-
-        System.out.println(getMapInformation());
     }
 
 
+    /**
+     * Aktion des Servers bei Beitritt eines weiteren Clients.
+     *
+     * @param pClientIP IP des neuen Clients.
+     * @param pClientPort Port des neuen Clients.
+     */
     @Override
     public void processNewConnection(String pClientIP, int pClientPort) {
         switch(numberOfClients){
@@ -53,6 +67,13 @@ public class TicTacToeServer extends Server {
         }
     }
 
+    /**
+     * Aktion des Servers bei einer Nachricht.
+     *
+     * @param pClientIP IP des aktuellen Clients.
+     * @param pClientPort Port des aktuellen Clients.
+     * @param pMessage Die empfangene Nachricht des Clients an den Server.
+     */
     @Override
     public void processMessage(String pClientIP, int pClientPort, String pMessage) {
         if(pMessage.contains("ACTION")){
@@ -78,8 +99,65 @@ public class TicTacToeServer extends Server {
             }
         }
         sendToAll("UPDATE"+getMapInformation());
+        for(int i=0;i<map.length && !playerWon;i++){
+            for(int j=0;j<map[i].length && !playerWon;j++){
+                if((map[i][0].getValue()==1 && map[i][1].getValue()==1 && map[i][2].getValue()==1)){
+                    sendToAll("WIN1");
+                    sendToAll("TEXTSpieler 1 hat gewonnen!");
+                    playerWon = true;
+                }
+                if((map[0][j].getValue()==1 && map[1][j].getValue()==1 && map[2][j].getValue()==1)){
+                    sendToAll("WIN1");
+                    sendToAll("TEXTSpieler 1 hat gewonnen!");
+                    playerWon = true;
+
+                }
+                if(map[0][0].getValue()==1 && map[1][1].getValue() == 1 && map[2][2].getValue() == 1){
+                    sendToAll("WIN1");
+                    sendToAll("TEXTSpieler 1 hat gewonnen!");
+                    playerWon = true;
+
+                }
+                if(map[0][2].getValue() == 1 && map[1][1].getValue() == 1 && map[2][0].getValue() == 1){
+                    sendToAll("WIN1");
+                    sendToAll("TEXTSpieler 1 hat gewonnen!");
+                    playerWon = true;
+
+                }
+                if((map[i][0].getValue()==2 && map[i][1].getValue()==2 && map[i][2].getValue()==2)){
+                    sendToAll("WIN2");
+                    sendToAll("TEXTSpieler 2 hat gewonnen!");
+                    playerWon = true;
+
+                }
+                if((map[0][j].getValue()==2 && map[1][j].getValue()==2 && map[2][j].getValue()==2)){
+                    sendToAll("WIN2");
+                    sendToAll("TEXTSpieler 2 hat gewonnen!");
+                    playerWon = true;
+
+                }
+                if(map[0][0].getValue()==2 && map[1][1].getValue() == 2 && map[2][2].getValue() == 2){
+                    sendToAll("WIN2");
+                    sendToAll("TEXTSpieler 2 hat gewonnen!");
+                    playerWon = true;
+
+                }
+                if(map[0][2].getValue() == 2 && map[1][1].getValue() == 2 && map[2][0].getValue() == 2){
+                    sendToAll("WIN2");
+                    sendToAll("TEXTSpieler 2 hat gewonnen!");
+                    playerWon = true;
+
+                }
+            }
+        }
     }
 
+    /**
+     * Aktion des Servers, wenn ein Client die Sitzung verlässt.
+     *
+     * @param pClientIP IP des verlassenden Clients.
+     * @param pClientPort Port des verlassenden Clients.
+     */
     @Override
     public void processClosingConnection(String pClientIP, int pClientPort) {
         switch(numberOfClients){
@@ -94,6 +172,9 @@ public class TicTacToeServer extends Server {
         }
     }
 
+    /**
+     * Zufällige Vergabe des ersten Spielzuges.
+     */
     private void firstTurn(){
         if(numberOfClients == 2){
             int temp = (int)(Math.random()*2+1);
@@ -105,6 +186,9 @@ public class TicTacToeServer extends Server {
         }
     }
 
+    /**
+     * Erstellung eines leeren Spielfeldes.
+     */
     private void createMap(){
         for(int i=0;i<map.length;i++){
             for(int j=0;j<map[i].length;j++){
@@ -114,6 +198,13 @@ public class TicTacToeServer extends Server {
     }
 
 
+    /**
+     * Gibt das Spielfeld in Form eines Strings zurück.
+     * Ein Feld wird mit dem Stichwort 'NEXT' unterschieden.
+     * Eine Feldinformation wird mit dem Stichwort 'FIELD' unterschieden.
+     *
+     * @return Aktuelles Spielfeld im String verkettet.
+     */
     private String getMapInformation(){
         String mapInfo = "";
         for(int i=0;i<map.length;i++){
